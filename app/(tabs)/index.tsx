e51cb5a93c097999a1fc120c6fab98ee58cf01c1
ezrahel <adelakinisrael024@gmail.com>
2025-04-06 21:32:11 +0100

    feat: implemented the user story feature

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,45 @@
-import { Text, View, Image } from "react-native";
-import {styles} from '../../styles/auth.styles'
+import { Text, View, Image, TouchableOpacity, ScrollView } from "react-native";
+import {styles} from '../../styles/feed.styles'
 import {Link} from 'expo-router'
+import { useClerk } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { STORIES } from "@/constants/mock-data";
+import { COLORS } from "@/constants/theme";
+import Story from "@/components/story";
+import * as Linking from 'expo-linking'
 export default function index() {
+    const { signOut } = useClerk()
+      
+    const handleSignOut = async () => {
+      try {
+        await signOut()
+        // Redirect to your desired page
+        Linking.openURL(Linking.createURL('/(auth)/login'))
+      } catch (err) {
+        // See https://clerk.com/docs/custom-flows/error-handling
+        // for more info on error handling
+        console.error(JSON.stringify(err, null, 2))
+      }
+    }
   return (
     <View
     style={styles.container}
     >
-      <Text >Edit app/index.tsx to edit this screen, Israel.</Text>
-     
-     
-        <Link href="/notifications">
-          Feed screen
-          feddddddddddddddddddddddddddddddddddddddd
-        </Link>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>
+          Memoir-Z
+        </Text>
+        <TouchableOpacity onPress={handleSignOut}>
+          <Ionicons name="log-out-outline" size={24} color={COLORS.white}/>
+        </TouchableOpacity>
+      </View>
+       <ScrollView showsVerticalScrollIndicator={false}>
+        <ScrollView horizontal
+        showsHorizontalScrollIndicator={false}
+        style={styles.storiesContainer}>
+          {STORIES.map((story)=>(<Story key={story.id} story={story}/>))}
+        </ScrollView>
+       </ScrollView>
     </View>
   );
 }
---
+++ constants/mock-data.ts
@@ -0,0 +1,62 @@
+export const STORIES = [
+    {
+        id:"1",
+        username: "You",
+        avatar: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=400&fit=crop",
+        hasStory: true,
+    },
+    {
+        id:"2",
+        username: "Abimelech",
+        avatar: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&h=400&fit=crop",
+        hasStory: true,
+    },
+    {
+        id:"3",
+        username: "Korah",
+        avatar: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=400&h=400&fit=crop",
+        hasStory: true,
+    },
+    {
+        id:"4",
+        username: "Absalom",
+        avatar: "https://images.unsplash.com/photo-11517841905240-472988babdf9?w=400&h=400&fit=crop",
+        hasStory: false,
+    },
+    {
+        id:"5",
+        username: "Achan.cumi",
+        avatar: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=400&h=400&fit=crop",
+        hasStory: false,
+    },
+    {
+        id:"6",
+        username: "japhet",
+        avatar: "https://images.unsplash.com/photo-158629135537-94bc9ba060aa?w=400&h=400&fit=crop",
+        hasStory: false,
+    },
+    {
+        id:"7",
+        username: "Nebucadnezzar",
+        avatar: "https://images.unsplash.com/photo-1701615004837-40d8573b6652?w=400&h=400&fit=crop",
+        hasStory: false,
+    },
+    {
+        id:"8",
+        username: "Jehohakim",
+        avatar: "https://images.unsplash.com/photo-163332755192-727a05c4013d?w=400&h=400&fit=crop",
+        hasStory: true,
+    },
+    {
+        id:"9",
+        username: "Aaron",
+        avatar: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=400&fit=crop",
+        hasStory: true,
+    },
+    {
+        id:"10",
+        username: "Eleazar",
+        avatar: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=400&fit=crop",
+        hasStory: true,
+    },
+]
